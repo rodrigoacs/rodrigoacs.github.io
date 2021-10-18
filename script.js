5244f4a94e4b7c8c8f8a40f1d3a67fa446e6a444
Rodrigo <rodrigohths@gmail.com>
2021-10-17 21:39:31 -0300

    reform

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 function toggleDark() {
-    var element = document.body;
-    element.classList.toggle("light");
+    var mainElement = document.getElementById("mainElement");
+    mainElement.classList.toggle("light");
+
+    var lightSectionAboutMe = document.getElementById("sectionAboutMe");
+    lightSectionAboutMe.classList.toggle("sectionStyle");
+    lightSectionAboutMe.classList.toggle("lightGlass");
+
+    var lightSectionProjects = document.getElementById("sectionProjects");
+    lightSectionProjects.classList.toggle("sectionStyle");
+    lightSectionProjects.classList.toggle("lightGlass");
+
+    var lightSectionContact = document.getElementById("sectionContact");
+    lightSectionContact.classList.toggle("sectionStyle");
+    lightSectionContact.classList.toggle("lightGlass");
+
+    var lightNav = document.getElementById("navStyle");
+    lightNav.classList.toggle("navLight");
+
+    var lightLink = document.getElementById("linkOne");
+    lightLink.classList.toggle("navLink");
+    lightLink.classList.toggle("navLightLink");
+
+    lightLink = document.getElementById("linkTwo");
+    lightLink.classList.toggle("navLink");
+    lightLink.classList.toggle("navLightLink");
+
+    lightLink = document.getElementById("linkThree");
+    lightLink.classList.toggle("navLink");
+    lightLink.classList.toggle("navLightLink");
+
 }
