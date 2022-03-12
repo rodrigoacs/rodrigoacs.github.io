89170e7360f9a3db553a6c4e949081df313c0abc
Rodrigo Soares <rodrigohths@gmail.com>
2022-03-11 21:14:15 -0300

    fix geral
    correção no js, links das redes sociais funcionando, estilização do botão de envio do formulário de email

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+let counter;
+
 function rgbOn() {
-    document.getElementById('rgbOn').disabled = true
-    let i = 0
-    setInterval(() => {
-        document.body.style.setProperty('--color', `hsl(${i},100%,50%)`)
-        i++
-        if (i == 360) i = 0
-    }, 10);
+    let hueValue = 0
+    clearInterval(counter)
+    counter = setInterval(() => {
+        document.body.style.setProperty('--color', `hsl(${hueValue},100%,50%)`)
+        hueValue++
+        if (hueValue == 360) hueValue = 0
+    }, 5);
 }
 
