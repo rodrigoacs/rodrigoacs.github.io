be6a6c558c498668cace8ed8ada0b3e795b704ee
Rodrigo Soares <rodrigohths@gmail.com>
2022-04-14 16:25:41 -0300

    tabela-livros

--- projects/tabelaLivros.js
+++ projects/tabelaLivros.js
@@ -1,27 +1,14 @@
 fetch('https://rodrigoacs.github.io/projects/livros.json').then(response => response.json()).then(data => {
-    for (let i = 2020; i <= 2022; i++) {
-        // cria a tabela e da o ID referente ao ano
+    Object.entries(data).forEach(([year, months]) => {
         const table = document.body.appendChild(document.createElement('table'));
-        table.id = i;
+        table.appendChild(document.createElement('tr')).innerHTML = `<th colspan="99">${year}</th>`;
+        table.appendChild(document.createElement('tr')).innerHTML = `<th>Mês</th><th>Qtde</th><th colspan="99">Livros</th>`;
 
-        // cria o cabeçalho com o ano 
-        const tableYear = document.createElement('th')
-        tableYear.innerHTML = i
-        tableYear.setAttribute('colspan', '99')
-        table.appendChild(tableYear)
-
-        // cria cada linha com o mês e os livros lidos
-        for (let j = 0; j <= 11; j++) {
-            const tableRow = table.appendChild(document.createElement('tr'));
-            Object.values(data[i][j]).forEach(livro => {
-                if (livro != null && typeof livro != 'number') {
-                    var elem = document.createElement('td');
-                    elem.innerHTML = livro;
-                    tableRow.appendChild(elem);
-                }
+        months.forEach(month => {
+            const tr = table.appendChild(document.createElement('tr'));
+            Object.values(month).forEach(book => {
+                tr.appendChild(document.createElement('td')).innerHTML = book;
             })
-        }
-    }
+        })
+    })
 })
-
-
