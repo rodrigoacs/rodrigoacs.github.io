0a3ab808b69227a2095ff73b4ee847d05975d97e
Rodrigo Soares <rodrigohths@gmail.com>
2022-04-17 13:08:08 -0300

    add +1 livro

--- projects/tabelaLivros.js
+++ projects/tabelaLivros.js
@@ -1,14 +1,18 @@
-fetch('https://rodrigoacs.github.io/projects/livros.json').then(response => response.json()).then(data => {
-    Object.entries(data).forEach(([year, months]) => {
-        const table = document.body.appendChild(document.createElement('table'));
-        table.appendChild(document.createElement('tr')).innerHTML = `<th colspan="99">${year}</th>`;
-        table.appendChild(document.createElement('tr')).innerHTML = `<th>Mês</th><th>Qtde</th><th colspan="99">Livros</th>`;
+fetch("https://rodrigoacs.github.io/projects/livros.json")
+    .then(response => response.json())
+    .then(data => {
+        Object.entries(data).forEach(([year, months]) => {
+            const table = document.body.appendChild(document.createElement("table"));
+            table.appendChild(document.createElement("tr")).innerHTML = `<th colspan="99">${year}</th>`;
+            table.appendChild(document.createElement("tr")).innerHTML = `<th>Mês</th><th>Qtde</th><th colspan="99">Livros</th>`;
 
-        months.forEach(month => {
-            const tr = table.appendChild(document.createElement('tr'));
-            Object.values(month).forEach(book => {
-                tr.appendChild(document.createElement('td')).innerHTML = book;
-            })
-        })
-    })
-})
+            months.forEach(month => {
+                const tr = table.appendChild(document.createElement("tr"));
+                Object.values(month).forEach(book => {
+                    tr.appendChild(document.createElement("td")).innerHTML = book;
+                });
+            });
+        });
+    });
+
+
