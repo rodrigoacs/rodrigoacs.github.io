2064079c5ed3d64379d7b4cdc12023e48fdb79b1
Rodrigo Soares <rodrigohths@gmail.com>
2022-04-22 22:01:32 -0300

    ajax-nav

--- projects/site-novo-dnv/script.js
+++ projects/site-novo-dnv/script.js
@@ -1,10 +1,22 @@
 document.querySelectorAll("a").forEach(function (link) {
     link.addEventListener('click', (e) => {
-        document.title = `Rodrigo | ${e.target.innerText[0].toUpperCase() + e.target.innerText.slice(1)}`;
+        // navegação via AJAX
+        const url = window.location.href.split("/");
+        url.pop()
+        const destination = e.target.attributes["nav-id"].value;
+        fetch(url.join('/').concat(`/${destination}.html`))
+            .then(response => response.text())
+            .then(content => {
+                document.getElementById("link-area").innerHTML = content
+            })
+
+        // mudança do titulo da página
+        document.title = `Rodrigo | ${e.target.innerText}`;
+
+        // estilo dos botões de navegação
         document.querySelectorAll("a").forEach(function (link) {
             link.classList.remove("ativado");
         })
-
         if (e.target.tagName == "SPAN") {
             e.target.parentNode.classList.add("ativado");
         } else {
