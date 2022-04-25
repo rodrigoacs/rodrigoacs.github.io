ab188554d18671d04e46a448822d6d252d0b01ba
Rodrigo Soares <rodrigohths@gmail.com>
2022-04-25 11:44:32 -0300

    js separado em funções

--- projects/site-novo-dnv/script.js
+++ projects/site-novo-dnv/script.js
@@ -1,27 +1,37 @@
-document.querySelectorAll("a").forEach(function (link) {
-    link.addEventListener('click', (e) => {
-        // navegação via AJAX
-        const url = window.location.href.split("/");
-        url.pop()
-        const destination = e.target.attributes["nav-id"].value;
-        fetch(url.join('/').concat(`/${destination}.html`))
-            .then(response => response.text())
-            .then(content => {
-                document.getElementById("link-area").innerHTML = content
-            })
-
-        // mudança do titulo da página
-        document.title = `Rodrigo | ${e.target.innerText}`;
-
-        // estilo dos botões de navegação
-        document.querySelectorAll("a").forEach(function (link) {
-            link.classList.remove("ativado");
-        })
-        if (e.target.tagName == "SPAN") {
-            e.target.parentNode.classList.add("ativado");
-        } else {
-            e.target.classList.add("ativado");
-        }
-
+document.querySelectorAll(".nav-buttons").forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        ajaxNavigation(e.target);
+        changeTitle(e.target);
+        highligthLink(e.target);
     });
 });
+
+function ajaxNavigation(click) {
+    const url = window.location.href.split("/");
+    url.pop();
+    const destination = click.attributes["nav-id"].value;
+    fetch(`${url.join("/")}/${destination}.html`)
+        .then((response) => response.text())
+        .then((content) => {
+            document.getElementById("link-area").innerHTML = content;
+        });
+}
+
+function changeTitle(click) {
+    document.title = `Rodrigo | ${click.innerText}`;
+    if (click.innerText === "home") { document.title = "Rodrigo | Home"; }
+    if (click.innerText === "person") { document.title = "Rodrigo | Sobre"; }
+    if (click.innerText === "folder_open") { document.title = "Rodrigo | Projetos"; }
+    if (click.innerText === "mail") { document.title = "Rodrigo | Contato"; }
+}
+
+function highligthLink(click) {
+    document.querySelectorAll(".nav-buttons").forEach(function (link) {
+        link.classList.remove("ativado");
+    });
+    if (click.tagName === "SPAN") {
+        click.parentNode.classList.add("ativado");
+    } else {
+        click.classList.add("ativado");
+    }
+}
