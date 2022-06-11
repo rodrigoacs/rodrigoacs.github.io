14c9322155611efd10b0ae16317a85a640db1ff3
Rodrigo Soares <rodrigohths@gmail.com>
2022-06-11 17:07:11 -0300

    #7daysofcode
    responsividade, arrumei a bola q n ficava redonda

--- projects/7-days-of-code/script.js
+++ projects/7-days-of-code/script.js
@@ -1,60 +1,60 @@
 window.onload = getPopularMovies();
 
 function renderMovies(movieTBR) {
-    const movie = document.createElement('div')
-    movie.classList.add('movie')
+    const movie = document.createElement("div")
+    movie.classList.add("movie")
 
-    const movieInfos = document.createElement('div')
-    movieInfos.classList.add('movie-infos')
+    const movieInfos = document.createElement("div")
+    movieInfos.classList.add("movie-infos")
     movie.appendChild(movieInfos)
 
-    const moviePoster = document.createElement('div')
-    moviePoster.classList.add('movie-poster')
+    const moviePoster = document.createElement("div")
+    moviePoster.classList.add("movie-poster")
     moviePoster.style.backgroundImage = `url(${movieTBR.image})`
     movieInfos.appendChild(moviePoster)
 
-    const movieTrf = document.createElement('div')
-    movieTrf.classList.add('movie-trf')
+    const movieTrf = document.createElement("div")
+    movieTrf.classList.add("movie-trf")
     movieTrf.innerHTML = `<span>${movieTBR.title} (${movieTBR.year})</span>`
     movieInfos.appendChild(movieTrf)
 
-    const movieRating = document.createElement('div')
-    movieRating.classList.add('movie-rating')
+    const movieRating = document.createElement("div")
+    movieRating.classList.add("movie-rating")
 
-    const starIcon = document.createElement('img')
-    starIcon.src = './images/star-icon.png'
+    const starIcon = document.createElement("img")
+    starIcon.src = "./images/star-icon.png"
     starIcon.style.width = `2.4rem`
     movieRating.appendChild(starIcon)
     movieRating.innerHTML += `<span> ${movieTBR.rating}</span>`
 
-    const movieFavorite = document.createElement('div')
-    movieFavorite.classList.add('favorite')
-    const favoriteButton = document.createElement('button')
-    const favoriteImage = document.createElement('img')
+    const movieFavorite = document.createElement("div")
+    movieFavorite.classList.add("favorite")
+    const favoriteButton = document.createElement("button")
+    const favoriteImage = document.createElement("img")
     if (movieTBR.isFavorited) {
-        favoriteImage.src = './images/filled-heart-icon.png'
+        favoriteImage.src = "./images/filled-heart-icon.png"
     } else {
-        favoriteImage.src = './images/empty-heart-icon.svg'
+        favoriteImage.src = "./images/empty-heart-icon.svg"
     }
     favoriteImage.style.width = `2.4rem`
-    const favoriteText = document.createElement('span')
-    favoriteText.innerHTML = ' Favoritar'
+    const favoriteText = document.createElement("span")
+    favoriteText.innerHTML = " Favoritar"
     favoriteButton.appendChild(favoriteImage)
     movieFavorite.appendChild(favoriteButton)
     movieFavorite.appendChild(favoriteText)
     movieTrf.appendChild(movieRating)
     movieTrf.appendChild(movieFavorite)
 
-    const movieDescription = document.createElement('div')
-    movieDescription.classList.add('movie-description')
-    movieDescription.innerHTML = `<span>${movieTBR.description}</span>`
+    const movieDescription = document.createElement("div")
+    movieDescription.classList.add("movie-description")
+    movieDescription.innerHTML = `${movieTBR.description}`
     movie.appendChild(movieDescription)
 
-    document.querySelector('.movies').appendChild(movie)
+    document.querySelector(".movies").appendChild(movie)
 }
 
 async function getPopularMovies() {
-    const url = 'https://api.themoviedb.org/3/movie/popular?api_key=04c83eacb1a1a16fb8cfc98c293b0b6d&language=pt-BR&page=1'
+    const url = "https://api.themoviedb.org/3/movie/popular?api_key=04c83eacb1a1a16fb8cfc98c293b0b6d&language=pt-BR&page=1"
     const fetchResponse = await fetch(url).then(response => response.json())
 
     let movies = []
@@ -63,7 +63,7 @@ async function getPopularMovies() {
             image: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
             title: movie.title,
             rating: movie.vote_average,
-            year: movie.release_date.split('-')[0],
+            year: movie.release_date.split("-")[0],
             description: movie.overview,
             isFavorited: false
         }
