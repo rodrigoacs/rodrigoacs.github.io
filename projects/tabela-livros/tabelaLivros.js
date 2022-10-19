b57e38e90c077bfebff4fe3597b04bd17ae1d2e6
Rodrigo Soares <rodrigohths@gmail.com>
2022-10-19 17:44:05 -0300

    search funcion improde

--- projects/tabela-livros/tabelaLivros.js
+++ projects/tabela-livros/tabelaLivros.js
@@ -2,51 +2,41 @@
 function createTable(bookInfos) {
     bookInfos = bookInfos.split(";");
     let tr = document.createElement("tr");
-    let td = document.createElement("td");
 
-    td.innerHTML = bookInfos[0];
-    tr.appendChild(td);
-    td = document.createElement("td");
-    td.innerHTML = bookInfos[1];
-    tr.appendChild(td);
-    td = document.createElement("td");
-    td.innerHTML = bookInfos[2];
-    td.id = bookInfos[2];
-    tr.appendChild(td);
-    td = document.createElement("td");
-    td.innerHTML = bookInfos[3];
-    tr.appendChild(td);
-    td = document.createElement("td");
-    td.innerHTML = bookInfos[4];
-    tr.appendChild(td);
-    td.innerHTML = bookInfos[5];
-    tr.appendChild(td);
+    for (let i = 0; i < 6; i++) {
+        let td = document.createElement("td");
+        td.innerHTML = bookInfos[i];
+        td.id = `${bookInfos[i]}`;
+        tr.appendChild(td);
+    }
+
     document.getElementById("table").appendChild(tr);
-
-
 }
 
 fetch("./Biblioteca-4780343c6bfa45e2bb9299516cf05e3c-2.csv")
     .then(response => response.text())
     .then(csv => csv.split(/\n/))
-    .then(books => books.forEach(createTable))
+    .then(books => books.slice(1).forEach(createTable))
 
-let query = document.getElementById("query");
-query.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("search").click();
-    }
-});
+document.querySelectorAll('input').forEach(input => {
+    input.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            search(this);
+        }
+    })
+}
+)
 
-document.querySelector("#search").addEventListener("click", searchAuthor);
-
-function searchAuthor() {
-    document.querySelectorAll(`tr`).forEach(element => {
-        if (element.children[2].id.toLowerCase() != query.value.toLowerCase()) {
-            element.style.display = "none";
-        } else {
+// reference.id = id que representa o que deve ser pesquisado
+// 0 data, 1 livro, 2 autor, 3 genero, 4 nota, 5 favorito;
+// reference.value = value do input que chamou a função
+function search(reference) {
+    document.querySelectorAll(`tr:not(#header)`).forEach(element => {
+        if (element.children[reference.id].id.toLowerCase().includes(reference.value.toLowerCase())) {
             element.style.display = "table-row";
+        } else {
+            element.style.display = "none";
         }
     });
 }
