7f2afdf57d4e46bc720cea6386467482ea1a873d
Rodrigo <rodrigohths@gmail.com>
2023-01-17 13:30:56 -0300

    fix: animation

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let counter;
 
 function rgbOn() {
-    let hueValue = 0
-    clearInterval(counter)
-    counter = setInterval(() => {
-        document.body.style.setProperty('--color', `hsl(${hueValue},100%,50%)`)
-        hueValue++
-        if (hueValue == 360) hueValue = 0
-    }, 5);
+  let hueValue = 0
+  clearInterval(counter)
+  counter = setInterval(() => {
+    document.body.style.setProperty('--color', `hsl(${hueValue},100%,50%)`)
+    hueValue++
+    if (hueValue == 360) hueValue = 0
+  }, 5);
 }
