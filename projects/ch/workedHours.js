79f491fd5376c4e0d1e4a4f7b1d104a9d2349ba2
Rodrigo <rodrigohths@gmail.com>
2023-04-21 16:50:21 -0300

    add: favicon, suporte a tarefas ilimitadas, caixa de texto para descricao

--- projects/ch/workedHours.js
+++ projects/ch/workedHours.js
@@ -1,22 +1,22 @@
-function calculate(initial, final) {
+function calculateIndividual(initial, final) {
   initial = new Date(`01/01/2023 ${initial}:00`).getTime()
   final = new Date(`01/01/2023 ${final}:00`).getTime()
 
   return (final - initial) / 60000
 }
 
-function calculate2() {
-  let intervals = document.getElementsByClassName('interval')
+function calculateAll() {
+  let intervals = document.getElementsByClassName("interval")
   let spentTime = 0
   for (let i = 0; i < intervals.length; i++) {
-    let initial = intervals[i].firstElementChild.value
-    let final = intervals[i].lastElementChild.value
+    let initial = intervals[i].children[0].value
+    let final = intervals[i].children[1].value
 
     if (!initial | !final) {
       break
     }
 
-    spentTime += calculate(initial, final)
+    spentTime += calculateIndividual(initial, final)
   }
   printResult(spentTime)
 }
@@ -24,5 +24,23 @@ function calculate2() {
 function printResult(time) {
   let hours = parseInt(time / 60)
   let minutes = time - hours * 60
-  document.getElementById('result').innerText = `${hours} horas e ${minutes} minutos`
+  document.getElementById("result").innerText = `${hours} horas e ${minutes} minutos`
+}
+
+function addActivity() {
+  let times = document.getElementsByClassName("times")
+  let div = document.createElement("div")
+  div.classList = "interval"
+  let inputTime = document.createElement("input")
+  inputTime.type = "time"
+  let inputTime2 = document.createElement("input")
+  inputTime2.type = "time"
+  let inputText = document.createElement("input")
+  inputText.type = "text"
+
+  div.appendChild(inputTime)
+  div.appendChild(inputTime2)
+  div.appendChild(inputText)
+
+  times[times.length - 1].appendChild(div)
 }
